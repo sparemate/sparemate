7bc54c2675fc02e8779a747162e9d8bce83c2d90
Shreyank Karjigi <sparematehosting@gmail.com>
2021-05-15 21:51:25 +0530

    Revert "host"
    This reverts commit d84fe0b24112ef81d1a285cf4097a629a3a298db.

--- shop/admin/HASH.py
+++
@@ -1,7 +0,0 @@
-for i in range(6):
-
-    for j in range(6):
-
-        print(j * "*")
-print(i * "*")
-
--- shop/static/js/index.js
+++
@@ -1,6 +0,0 @@
-
-  $("button").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".image1").offset().top},
-        'slow');
-});
